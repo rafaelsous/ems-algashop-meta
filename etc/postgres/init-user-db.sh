bd102a599b5d3d202c7e4e60a18f129e9d4817a4
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-04-22 16:32:25 -0300

    EMS-N-05-06.08 - Adding dependencies and configuring the application.yml file for the database in the authorization server microservice.

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -5,6 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE DATABASE fastpay;
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
-	CREATE DATABASE ordering_test;
 	CREATE DATABASE billing_test;
+	CREATE DATABASE authorization_server;
+	CREATE DATABASE ordering_test;
+	CREATE DATABASE authorization_server_test;
 EOSQL
